e4ec6cecb961d6d98647a1e87839f07671d1edc4
0HunterDreams1 <caupancristian13@gmail.com>
2025-05-15 12:55:49 -0300

    funciona la imagen de fondo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
     <head>
         @include("includes.head")
     </head>
-    <body>
+    <body style="background-image: url('{{ asset('images/fondo.webp') }}');">
         @include("includes.header")
         <div class="flex items-center justify-center-safe content-center mt-8">
             @yield("content")
