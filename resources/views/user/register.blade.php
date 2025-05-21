e27352b6e35949849744ae12b1b8854b138613c6
peraltamiriam26 <peraltamiriam26@gmail.com>
2025-05-21 20:41:18 -0300

    formulario registro, estilos

--- resources/views/user/register.blade.php
+++ resources/views/user/register.blade.php
@@ -4,68 +4,76 @@
   <form method="POST" action="{{ route('validate-register') }}">
     @csrf
     <div class="card-body flex items-center">
-      <div class="flex items-center" style="width: 100%; justify-content: center;">
-        <div style="position: absolute; left: 0;">
+      <!-- Flecha y titulo -->
+      <div class="grid grid-flow-col w-120">
+        <div class="">
           <a href="{{ url('/') }}" class="link link-primary no-underline">
             <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 21 21">
               <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" d="M7.499 6.497L3.5 10.499l4 4.001m9-4h-13" stroke-width="1" />
             </svg>
           </a>
         </div>
-        <h5 class="card-title">Registro</h5>
+        <div class="">
+          <h5 class="card-title">Registro</h5>
+        </div>
       </div>
-      <div class="w-145">
-
+      <!-- Comienza formulario -->
+      <div class="w-120">
         <div class="grid grid-flow-row">
-          <div class="grid grid-flow-col justify-items-center">
-            <label class="label-text">Nombre</label>
-            <label class="label-text">Apellido</label>
-          </div>
-          <div class="grid grid-flow-col">
-            <input type="text" name="nombre" placeholder="Nombre" value="{{ old('nombre') }}" class="input @error('nombre') is-invalid @enderror"  id="nombreUsuario" autofocus/>
-            <input type="text" name="apellido" placeholder="Apelido" value="{{ old('apellido') }}" class="input @error('apellido') is-invalid @enderror"  id="apellidoUsuario"/>
-          </div>
-        </div>
-
-        <div class="grid grid-flow-col justify-items-center">
-          @error('nombre')
-            <div class="alert alert-soft alert-error mt-2" role="alert">{{ $message }}</div>
-          @enderror
-          @error('apellido')
-            <div class="alert alert-soft alert-error mt-2" role="alert">{{ $message }}</div>
-          @enderror
-        </div>
-        
-        <div class="grid grid-flow-col">
-          <div class="col items-center justify-items-center">
-            <label>Tipo de usuario</label>
-            <div class="flex items-center">
-              <input type="radio" name="tipoUsuario" value="lector" checked />
-              <label for="lector" style="margin-left: 5px; margin-right: 5px;">Lector</label>
-              <input type="radio" name="tipoUsuario" value="escritor"/>
-              <label for="escritor" style="margin-left: 5px; margin-right; 5px;">Escritor</label>
+          <div class="grid grid-flow-col justify-items-left">
+            <div class="me-2">
+              <label class="label-text">Nombre</label>
+              <input type="text" name="nombre" placeholder="Nombre" value="{{ old('nombre') }}" class="input @error('nombre') is-invalid @enderror"  id="nombreUsuario" autofocus/>
+              @error('nombre')
+                <div class="alert alert-soft alert-error mt-2" role="alert">{{ $message }}</div>
+              @enderror
+            </div>
+            <div>
+              <label class="label-text">Apellido</label>
+              <input type="text" name="apellido" placeholder="Apelido" value="{{ old('apellido') }}" class="input @error('apellido') is-invalid @enderror"  id="apellidoUsuario"/>
+              @error('apellido')
+                <div class="alert alert-soft alert-error mt-2" role="alert">{{ $message }}</div>
+              @enderror
             </div>
           </div>
-  
-          <div class="col items-center justify-items-center">
-  
-            <label class="label-text">Correo electrónico</label>
-            <input id="email" name="email" type="text" placeholder="correo@mail.com"  value="{{ old('email') }}" class="input @error('email') is-invalid @enderror" id="emailUsuario" />
-            @error('email')
-              <div class="alert alert-soft alert-error mt-2" role="alert">{{ $message }}</div>
-            @enderror
-  
-          </div>
-        
         </div>
 
-        <div class="grid grid-flow-row">
-          <div class="grid grid-flow-col justify-items-center ">
-            <label class="label-text">Contraseña</label>
-            <label class="label-text">Repetir la contraseña</label>
+        <div class="grid grid-flow-col pt-2">
+          <div class="grid grid-flow-col justify-items-left">
+            <div>
+              <label>Tipo de usuario</label>
+              <div class="flex items-center">
+                <input type="radio" name="tipoUsuario" value="lector" checked />
+                <label for="lector" class="ms-2 me-3">Lector</label>
+                <input type="radio" name="tipoUsuario" value="escritor"/>
+                <label for="escritor" class="ms-2 me-3">Escritor</label>
+              </div>
+            </div>
+          </div>  
+        </div>
+<!-- Correo -->
+        <div class="grid grid-flow-col pt-2">
+          <div class="grid grid-flow-col justify-items-left">
+            <div>
+              <label class="label-text">Correo electrónico</label>
+              <input id="email" name="email" type="text" placeholder="correo@mail.com"  value="{{ old('email') }}" class="w-60 input @error('email') is-invalid @enderror" id="emailUsuario" />
+              @error('email')
+                <div class="alert alert-soft alert-error mt-2 w-80" role="alert">{{ $message }}</div>
+              @enderror
+            </div>
+          </div>  
+        </div>
+
+
+        <div class="grid grid-flow-col pt-2">
+          <div class="grid grid-flow-col justify-items-left ">
+            <div class="me-2">
+              <label class="label-text">Contraseña</label>
+              <input id="password" name="password" type="password" class="input @error('password') is-invalid @enderror" placeholder="Ingrese una contraseña" value="{{ old('password') }}"  />
+            </div>
           </div>
-          <div class="grid grid-flow-col">
-            <input id="password" name="password" type="password" class="input @error('password') is-invalid @enderror" placeholder="Ingrese una contraseña" value="{{ old('password') }}"  />
+          <div>
+            <label class="label-text">Repetir la contraseña</label>
             <input id="password_confirmation" name="password_confirmation" type="password" class="input @error('password') is-invalid @enderror" placeholder="Ingrese una contraseña" value="{{ old('password_confirmation') }}" />
           </div>
         </div>
@@ -74,7 +82,9 @@
           <div class="alert alert-soft alert-error mt-2" role="alert">{{ $message }}</div>
         @enderror
       </div>
-      <input class="btn btn-primary grid grid-flow-col justify-items-center items-center-safe" type="submit" value="Registrarse">
+      <div class="pt-2">
+        <input class="btn btn-primary grid grid-flow-col justify-items-center items-center-safe rounded-full" type="submit" value="Registrarse">
+      </div>
     </div>
   </form>
 </div>
