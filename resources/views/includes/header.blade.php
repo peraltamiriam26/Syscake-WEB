f0fef150c70fe0ba531515b25abbbb5a9763f6ae
Miriam Peralta <peraltamiriam26@gmail.com>
2025-05-31 14:49:51 -0300

    model, controller plan

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -5,6 +5,16 @@
 <nav class="navbar w-full shadow-base-300/20 shadow-sm pb-6">
   <div class="w-full md:flex md:items-center md:gap-2">
     <div id="navbar-collapse" class="md:navbar-end collapse hidden grow basis-full overflow-hidden transition-[height] duration-300 max-md:w-full" >
+      <li class="pe-2 dropdown relative inline-flex [--auto-close:inside] [--offset:8] [--placement:bottom-end]"> 
+        <button id="dropdown-link" type="button" class="dropdown-toggle dropdown-open:bg-base-content/10 dropdown-open:text-base-content" aria-haspopup="menu" aria-expanded="false" aria-label="Dropdown" >
+          Planner
+          <span class="icon-[tabler--chevron-down] dropdown-open:rotate-180 size-4"></span>
+        </button>  
+        <ul class="dropdown-menu dropdown-open:opacity-100 hidden" role="menu" aria-orientation="vertical" aria-labelledby="dropdown-link" >
+          <li><a class="dropdown-item" href="{{route('create-plan')}}">Crear</a></li>
+          <li><a class="dropdown-item" href="{{route('index-plan')}}">Ver listado</a></li>
+        </ul>  
+      </li>
       <li class="pe-2 dropdown relative inline-flex [--auto-close:inside] [--offset:9] [--placement:bottom-end]">
         <button id="dropdown-end" type="button" class="dropdown-toggle dropdown-open:bg-base-content/10 dropdown-open:text-base-content max-md:px-2" aria-haspopup="menu" aria-expanded="false" aria-label="Dropdown">
           Administración
